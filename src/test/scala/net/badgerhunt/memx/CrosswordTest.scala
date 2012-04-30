ccf9a029b3829d8bef20fd00a22c7714448e92b5
Jem Mawson <jem.mawson@gmail.com>
2012-04-30 23:13:59 +1000

    placing words

--- src/test/scala/net/badgerhunt/memx/CrosswordTest.scala
+++ src/test/scala/net/badgerhunt/memx/CrosswordTest.scala
@@ -5,7 +5,7 @@ import org.specs2.mutable.Specification
 class CrosswordTest extends Specification {
 
   "A crossword wider than it is high" should {
-    val crossword = new Crossword(4,3)
+    val crossword = Crossword(4,3)
 
     "find no placements for words that are too large" in {
       crossword.placementsFor("しゅくだい") must beEmpty
@@ -35,7 +35,7 @@ class CrosswordTest extends Specification {
   }
 
   "A crossword higher than it is wide" should {
-    val crossword = new Crossword(3,4)
+    val crossword = Crossword(3,4)
 
     "find no placements for words that are too large" in {
       crossword.placementsFor("しゅくだい") must beEmpty
@@ -63,5 +63,61 @@ class CrosswordTest extends Specification {
         Placement(0, 3, Horizontal)
       ).only
     }
+  }
+
+  "A crossword" should {
+    val crossword = Crossword(10,10)
+
+    "allow a placement to be made" in {
+      val word = Word("パチンコ", "pachinko", Placement(3, 3, Horizontal))
+      val updated = crossword.place(word)
+      updated.words must contain(word).only
+    }
+
+    "allow a subsequent placement to be made" in {
+      val updated = crossword.place(
+        Word("もちろん", "of course", Placement(3, 8, Horizontal))).place(
+        Word("いりくち", "entrance", Placement(4, 5, Vertical)))
+      updated.words must contain(
+        Word("もちろん", "of course", Placement(3, 8, Horizontal)),
+        Word("いりくち", "entrance", Placement(4, 5, Vertical))).only
+      // todo - test if all the letters are correclty in place in the grid.
+    }
+
+    "deny a placement that overwrites an existing letter with a different one" in {
+      val updated = crossword.place(Word("もちろん", "of course", Placement(3, 3, Horizontal)))
+      val result = updated.place(Word("ことば", "word", Placement(4, 2, Vertical)))
+      result must beEqualTo(updated)
+    }
+
+    /*
+        "allow a placement that intersects an existing word with the same letter" in {
+
+        }
+
+        "deny a placement that breaches the left hand boundary" in {
+
+        }
+
+        "deny a placement that breaches the right hand boundary" in {
+
+        }
+
+        "deny a placement that breaches the top boundary" in {
+
+        }
+
+        "deny a placement that breaches the bottom boundary" in {
+
+        }
+
+        "deny a placement that breaches the top and bottom boundaries" in {
+
+        }
+
+        "deny a placement that breaches the left and right hand boundaries" in {
+
+        }
+    */
   }
 }
